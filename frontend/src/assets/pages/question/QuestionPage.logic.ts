ecb9811ab91a518af89a8c4adb7f6b059e53ae96
Luhito <www.kwmr@gmail.com>
2026-06-18 13:25:32 +0900

    画面表示のためAPI呼び出しをスタブ化

--- frontend/src/assets/pages/question/QuestionPage.logic.ts
+++ frontend/src/assets/pages/question/QuestionPage.logic.ts
@@ -1,24 +1,35 @@
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import type { QuestionPageState } from './QuestionPage.state';
-import type { GetQuestionResponse } from '@/api/generated/api';
-import { useQuery } from '@tanstack/react-query';
-import { API } from '@/api/wrappers/api';
+// import type { GetQuestionResponse } from '@/api/generated/api';
+// import { useQuery } from '@tanstack/react-query';
+// import { API } from '@/api/wrappers/api';
 
 export const useQuestionPageLogic = (states: QuestionPageState) => {
     const navigate = useNavigate();
     const { t } = useTranslation("question");
-    const api = new API();
-    const correct_answer_index = 1;
+    // const api = new API();
 
-    const qid = "sample";
-    const userAgent = "default";
-    const acceptLanguage = "default";
+    // const qid = "sample";
+    // const userAgent = "default";
+    // const acceptLanguage = "default";
 
-    const {status, data} = useQuery<GetQuestionResponse>({
-        queryKey: [ "question", qid, acceptLanguage ],
-        queryFn: () => api.getQuestion(qid, userAgent, acceptLanguage)
-    })
+    // const {status, data} = useQuery<GetQuestionResponse>({
+    //     queryKey: [ "question", qid, acceptLanguage ],
+    //     queryFn: () => api.getQuestion(qid, userAgent, acceptLanguage)
+    // })
+    const status = "stub"
+    const data = {
+        question_text: "サンプル問題文",
+        choices: [
+            {tag: "A", text: "サンプル選択肢1"},
+            {tag: "B", text: "サンプル選択肢2"},
+            {tag: "C", text: "サンプル選択肢3"},
+            {tag: "D", text: "サンプル選択肢4"},
+        ],
+        explanation_text: "サンプル解説文",
+        correct_answer_index: 1
+    }
 
     const onClick_viewAnswer = () => {
         if (states.isOpenAnswer) return;
@@ -39,7 +50,7 @@ export const useQuestionPageLogic = (states: QuestionPageState) => {
         if (states.isOpenAnswer) return;
 
         states.setOpenAnswer(true);
-        if (index === correct_answer_index) {
+        if (index === data.correct_answer_index) {
             alert("correct");
         }
         else {
