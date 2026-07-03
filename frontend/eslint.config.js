b3c1df10ffc8dde848f2a9b72981825b7621a915
Luhito <www.kwmr@gmail.com>
2026-07-03 12:19:52 +0900

    generatedの場所を変えて、tsconfigの設定を変えました　たのむて

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -20,7 +20,7 @@ export default defineConfig([
     },
   },
   {
-    files: ["src/api/generated/**/*.ts"],
+    files: ["generated/**/*.ts"],
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
